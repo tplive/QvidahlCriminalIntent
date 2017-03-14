43080220650313f91ef946b05583d1652a1a7c08
Thomas Qvidahl <tp@live.no>
2017-03-14 22:29:04 +0100

    Initial commit

--- app/src/main/java/no/qvidahl/qvidahlcriminalintent/Crime.java
+++ app/src/main/java/no/qvidahl/qvidahlcriminalintent/Crime.java
@@ -1,8 +1,30 @@
 package no.qvidahl.qvidahlcriminalintent;
 
+import java.util.UUID;
+
 /**
  * Created by Thomas on 14.03.2017.
  */
 
 public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        // Generate unique ID
+        mId = UUID.randomUUID();
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
 }
