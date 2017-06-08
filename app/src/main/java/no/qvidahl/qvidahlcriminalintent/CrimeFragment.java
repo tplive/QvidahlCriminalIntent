22121d8bc3e07a39af71c079cf0ac70e3a79da7e
Thomas Qvidahl <tp@live.no>
2017-06-08 21:57:23 +0200

    Trodding along. Page 138.

---
+++ app/src/main/java/no/qvidahl/qvidahlcriminalintent/CrimeFragment.java
@@ -0,0 +1,28 @@
+package no.qvidahl.qvidahlcriminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Thomas on 08.06.2017.
+ */
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fra)
+    }
+
+}
